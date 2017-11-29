5405804ef1898bad04ab5b8a892f9f0e9cf8684e
Stefan Johnson <sj@scrimpycat.io>
2017-11-30 09:13:43 +1100

    Update to 0.4.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SimpleMarkdown.Mixfile do
         [
             app: :simple_markdown,
             description: "A simple and extendable Markdown converter",
-            version: "0.3.1",
+            version: "0.4.0",
             elixir: "~> 1.2",
             build_embedded: Mix.env == :prod,
             start_permanent: Mix.env == :prod,
