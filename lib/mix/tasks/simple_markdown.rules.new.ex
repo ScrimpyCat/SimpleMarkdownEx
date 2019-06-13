6a35877cfef9bcdd23a23bf97b78dddbbec85abe
Stefan Johnson <sj@scrimpycat.io>
2019-06-13 17:04:18 +1000

    Copy rules from new rule file

--- lib/mix/tasks/simple_markdown.rules.new.ex
+++ lib/mix/tasks/simple_markdown.rules.new.ex
@@ -14,11 +14,28 @@ defmodule Mix.Tasks.SimpleMarkdown.Rules.New do
 
     use Mix.Task
 
-    def run(_) do
-        if File.cp_r!(Path.join(__DIR__, "simple_markdown_rules.exs"), "config/simple_markdown_rules.exs", fn _, destination ->
-            Mix.shell.yes?("Do you want to replace the file at #{destination}")
-        end) |> Enum.count == 1 do
-            Mix.shell.info "Please import the simple_markdown_rules.exs config."
+    def run(env) do
+        { _, { destination, formatter } } = Enum.reduce(env, { nil, { "config/simple_markdown_rules.exs", &(&1) } }, fn
+            destination, { :destination, { _, formatter } } -> { nil, { destination, formatter } }
+            "--destination", { _, args } -> { :destination, args }
+            "--format", { _, { destination, formatter } } -> { nil, { destination, if(Version.compare(System.version, "1.6.0") != :lt, do: &Code.format_string!/1, else: formatter) } }
+        end)
+
+        write_config(destination, formatter)
+    end
+
+    defp write_config(destination, formatter \\ &(&1)) do
+        with exists <- File.exists?(destination),
+             true <- if(exists, do: Mix.shell.yes?("Do you want to replace the file at #{destination}"), else: true) do
+                config = """
+                use Mix.Config
+
+                config :simple_markdown,
+                    rules: #{File.read!(Path.join(__DIR__, "../../simple_markdown/rules.exs"))}
+                """
+
+                File.write!(destination, formatter.(config))
+                Mix.shell.info "Please import the #{Path.basename(destination)} config."
         end
     end
 end
